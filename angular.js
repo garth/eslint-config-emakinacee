352c5eca0b276644b22760abc523bcb7ebf0b4d4
Daniel Gördes <d.goerdes@emakina.at>
2016-05-02 11:02:20 +0200

    change rules according to votes and discussion

--- angular.js
+++ angular.js
@@ -12,38 +12,38 @@ module.exports = {
         'angular/no-private-call': 2,
 
         // best practices
-        'angular/component-limit': [0, 1],
+        'angular/component-limit': 2,
         'angular/controller-as': 2,
-        'angular/controller-as-vm': [2, 'vm'],
+        'angular/controller-as-vm': 0,
         'angular/controller-as-route': 2,
-        'angular/deferred': 0, // research why new syntax...
+        'angular/deferred': 2,
         'angular/di-unused': 2,
-        'angular/directive-restrict': [2,{"explicit":"always"}], // always needs a restrict where A;E are valid only.
+        'angular/directive-restrict': [2, {"explicit": "always"}],
         'angular/empty-controller': 2,
-        'angular/no-controller': 2, // component first! disallow controllers.
-        'angular/no-inline-template': [2, {"allowSimple":false}], // no inline templates
+        'angular/no-controller': 2,
+        'angular/no-inline-template': [2, {"allowSimple":false}],
         'angular/no-run-logic': 0,
         'angular/no-services': [2, ['$http', '$resource']],
         'angular/on-watch': 2,
 
         // deprecated
         'angular/no-cookiestore': 2,
-        'angular/no-http-callback': 2, // disallow $http methods success/error
+        'angular/no-http-callback': 2,
         'angular/no-directive-replace': 2,
 
         // naming
-        'angular/controller-name': [2, '/[a-zA-Z].*ctrl$/'],
-        'angular/directive-name': [2,"dd"], // directive should be prefixed by dd -> ddTabs
-        'angular/file-name': 0,
+        'angular/file-name': 2,
+        'angular/controller-name': 0,
+        'angular/directive-name': 0,
         'angular/filter-name': 0,
         'angular/module-name': 0,
-        'angular/service-name': 2,
+        'angular/service-name': 0,
 
         // conventions
         'angular/di-order': [0, true],
         'angular/di': [2, 'function'],
         'angular/dumb-inject': 2,
-        'angular/function-type': 0,
+        'angular/function-type': [2, "named"],
         'angular/module-dependency-order': 2,
         'angular/no-service-method': 0,
         'angular/one-dependency-per-line': 0,
@@ -57,16 +57,16 @@ module.exports = {
         'angular/foreach': 0,
         'angular/interval-service': 2,
         'angular/json-functions': 2,
-        'angular/log': 2,
+        'angular/log': 1,
         'angular/no-angular-mock': 0,
         'angular/no-jquery-angularelement': 2,
         'angular/timeout-service': 2,
-        'angular/typecheck-array': 2,
-        'angular/typecheck-date': 2,
-        'angular/typecheck-function': 2,
-        'angular/typecheck-number': 2,
-        'angular/typecheck-object': 2,
-        'angular/typecheck-string': 2,
+        'angular/typecheck-array': 0,
+        'angular/typecheck-date': 0,
+        'angular/typecheck-function': 0,
+        'angular/typecheck-number': 0,
+        'angular/typecheck-object': 0,
+        'angular/typecheck-string': 0,
         'angular/window-service': 2
     }
 };
